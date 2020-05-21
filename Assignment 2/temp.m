d762f46567a11d379c97ba9b474c359bc7ff68a2
Johnson9128 <43661949+Johnson15177@users.noreply.github.com>
2020-05-21 22:20:00 +1000

    update

--- Assignment 2/temp.m
+++ Assignment 2/temp.m
@@ -6,5 +6,5 @@ fetchBase = transl(0,0,1);
 workspace = [-0.2 1 -0.5 0.5 -0.1 1.5];
 name = 'Robot';
 robot = Fetch(fetchBase, workspace, name);
-%robot.model.teach
+robot.model.teach
 %robot.Move(transl(0.2, 0.2, 0.1))
