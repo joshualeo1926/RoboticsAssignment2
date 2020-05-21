4d1927ff1d5f34e4989203701e820082bcee4684
Johnson9128 <43661949+Johnson15177@users.noreply.github.com>
2020-05-21 19:03:47 +1000

    Johnson

--- Assignment 2/Fetch.m
+++ Assignment 2/Fetch.m
@@ -2,7 +2,7 @@ classdef Fetch < handle
     properties
         model;
         base = transl(0,0,0);
-        workspace = [-2 2 -2 2 -0.8 4];
+        workspace = [-2 2 -2 2 -2 2];
         name = 'Robot';
         scale = 0.2;
     end
@@ -22,15 +22,16 @@ classdef Fetch < handle
             pause(0.001);
         	link1 = Link('d',0.107511,'a',0.120238,'alpha',pi/2,'qlim',[deg2rad(-92),deg2rad(92)]); %shoulderpan
             link2 = Link('d',0,'a',0,'alpha',pi/2,'qlim',[deg2rad(-87),deg2rad(70)]); % shoulder lift
-            link3 = Link('d',0.179933,'a',0,'alpha',pi/2); % upperarm roll
-            link4 = Link('d',-0.5,'a',0,'alpha',-pi/2,'qlim',[deg2rad(-129),deg2rad(129)]); %elbow flex
-            link5 = Link('d',0.5,'a',0,'alpha',pi/2); %forearm roll
-            link6 = Link('d',0.5,'a',0,'alpha',-pi/2,'qlim',[deg2rad(-125),deg2rad(125)]); %wrist flex
-            link7 = Link('d',0.5,'a',0,'alpha',pi/2); %wrist roll
+            link3 = Link('d',0.179933,'a',0,'alpha',-pi/2); % upperarm roll
+            link4 = Link('d',0,'a',0,'alpha',pi/2,'qlim',[deg2rad(-129),deg2rad(129)]); %elbow flex
+            link5 = Link('d',0.15712,'a',0,'alpha',pi/2); %forearm roll
+            link6 = Link('d',0.051119,'a',0,'alpha',-pi/2,'qlim',[deg2rad(-125),deg2rad(125)]); %wrist flex
+            link7 = Link('d',0.04237,'a',0,'alpha',0); %wrist roll
             qMatrix = [0 0 0 0 0 0 0];
             
             self.model = SerialLink([link1 link2 link3 link4 link5 link6 link7], 'name', self.name, 'base', self.base);
             self.model.plot(qMatrix, 'workspace', self.workspace, 'scale', self.scale, 'noarrow');
+            %self.model.plot(qMatrix, 'workspace', self.workspace,'noarrow');
         end
         
         function Move(self, pos)
--- Assignment 2/temp.m
+++ Assignment 2/temp.m
@@ -1,8 +1,9 @@
 clear
 clc
+close all
 
 fetchBase = transl(0,0,0);
-workspace = [-1.5 1.5 -1.5 1.5 -0.1 1.5];
+workspace = [-1.5 1.5 -1.5 1.5 -1.5 1.5];
 name = 'Robot';
 robot = Fetch(fetchBase, workspace, name);
 robot.model.teach
