87676c3f74cf49a87db33053afef5b3984e6aafd
joshualeo1926 <joshua.leo1926@gmail.com>
2020-05-21 19:27:27 +1000

    added model ploting to class

--- Assignment 2/Fetch.m
+++ Assignment 2/Fetch.m
@@ -16,6 +16,7 @@ classdef Fetch < handle
             end
             
             self.GetFetchRobot();
+            self.PlotAndColourRobot();
         end
         
         function GetFetchRobot(self)
@@ -32,6 +33,39 @@ classdef Fetch < handle
             self.model = SerialLink([link1 link2 link3 link4 link5 link6 link7], 'name', self.name, 'base', self.base);
             self.model.plot(qMatrix, 'workspace', self.workspace, 'scale', self.scale, 'noarrow');
             %self.model.plot(qMatrix, 'workspace', self.workspace,'noarrow');
+        end
+        
+        function PlotAndColourRobot(self)%robot,workspace)
+            for linkIndex = 0:self.model.n
+                currentFile = mfilename( 'fullpath' );
+                [pathstr,~,~] = fileparts( currentFile );
+                plyPath = fullfile(pathstr , '..', 'PLY', ['L',  num2str(linkIndex), '.ply']);
+                [ faceData, vertexData, plyData{linkIndex + 1} ] = plyread(plyPath,'tri');
+                self.model.faces{linkIndex + 1} = faceData;
+                self.model.points{linkIndex + 1} = vertexData;
+            end
+
+            % Display robot
+            self.model.plot3d(zeros(1,self.model.n),'noarrow','workspace',self.workspace);
+            if isempty(findobj(get(gca,'Children'),'Type','Light'))
+                camlight
+            end  
+            self.model.delay = 0;
+
+            % Try to correctly colour the arm (if colours are in ply file data)
+            for linkIndex = 0:self.model.n
+                handles = findobj('Tag', self.model.name);
+                h = get(handles,'UserData');
+                try 
+                    h.link(linkIndex+1).Children.FaceVertexCData = [plyData{linkIndex+1}.vertex.red ...
+                                                                  , plyData{linkIndex+1}.vertex.green ...
+                                                                  , plyData{linkIndex+1}.vertex.blue]/255;
+                    h.link(linkIndex+1).Children.FaceColor = 'interp';
+                catch ME_1
+                    disp(ME_1);
+                    continue;
+                end
+            end
         end
         
         function Move(self, pos)
