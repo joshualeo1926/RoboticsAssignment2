0704ee0a7fb211b4c27bf432b23ad8488cf736ad
Johnson9128 <43661949+Johnson15177@users.noreply.github.com>
2020-05-27 13:08:32 +1000

    GUI EStop

--- Assignment 2/temp.m
+++ Assignment 2/temp.m
@@ -15,4 +15,21 @@ gui = GUI();
 %%
 %gui.EStopButtonPushed()
 %gui.Lamp.Color = [0,1,0];
-state = gui.func()
+EStopValue = gui.getEStopValue()
+%%
+while(1)
+    paused = 1;
+    disp('Not paused');
+    pause(0.00001)
+    EStopValue = gui.getEStopValue();
+    if(EStopValue == 1)
+        while(paused == 1)
+            disp('paused');
+            pause(0.00001)
+            EStopValue = gui.getEStopValue();
+            if(EStopValue == 0)
+                paused = 0;
+            end
+        end
+    end
+end
