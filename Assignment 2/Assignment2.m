32d80ee286750519b461396c1bedee569f166baa
Johnson9128 <43661949+Johnson15177@users.noreply.github.com>
2020-05-29 00:15:07 +1000

    Gui Teach

--- Assignment 2/Assignment2.m
+++ Assignment 2/Assignment2.m
@@ -1,4 +1,4 @@
-function assignment_2()
+
 clear all;
 close all;
 clf;
@@ -37,65 +37,97 @@ robot = Fetch(fetchBase, workspace, name);
 q = deg2rad([92 -80 0 -100 0 85 0]);
 robot.model.plot(q, 'workspace', workspace, 'noarrow', 'scale', 0)
 gui = GUI();
-
+%%
 % Mail loop
 step = 1;
 while 1
     %read GUI
-    %check ESTOP
-    paused = 1;
     pause(0.00001)
-    EStopValue = gui.getEStopValue();
-    if(EStopValue == 1)
-        while(paused == 1)
+    powerState = gui.GetPowerValue()
+    while(powerState == "On")
+        pause(0.00001)
+        teachMode = gui.GetTeachModeValue();
+        if(teachMode == 1)
+            clc
+            clf
+            name = 'Robot';
+            workspace = [-1 1 -1 1 -0.1 1.5];
+            fetchBase = transl(0, 0, 0.52);
+            robot = Fetch(fetchBase, workspace, name);
+            q = deg2rad([92 -80 0 -100 0 85 0]);
+    robot.model.plot(q, 'workspace', workspace, 'noarrow', 'scale', 0);
+            while(teachMode == 1)
+                pause(0.00001)
+                teachMode = gui.GetTeachModeValue();
+                joint1 = gui.GetJoint1Value();
+                joint2 = gui.GetJoint2Value();
+                joint3 = gui.GetJoint3Value();
+                joint4 = gui.GetJoint4Value();
+                joint5 = gui.GetJoint5Value();
+                joint6 = gui.GetJoint6Value();
+                joint7 = gui.GetJoint7Value();
+                qMatrix = [joint1 joint2 joint3 joint4 joint5 joint6 joint7];
+                robot.model.plot(qMatrix);
+            end
+        end
+        % If Start is pressed start process
+        pause(0.00001)
+        startValue = gui.GetStartValue();
+        if(startValue == 1)
+            paused = 1;
+            %check ESTOP
             pause(0.00001)
-            EStopValue = gui.getEStopValue();
-            if(EStopValue == 0)
-                paused = 0;
+            EStopValue = gui.GetEStopValue();
+            if(EStopValue == 1)
+                while(paused == 1)
+                    pause(0.00001)
+                    EStopValue = gui.GetEStopValue();
+                    if(EStopValue == 0)
+                    paused = 0;
+                    end
+                end
+            end
+            
+            % move to workbench
+            if step == 1
+                destination = transl(workBenchPos(1, 4), workBenchPos(2, 4) - 0.95, fetchBase(3, 4))*trotz(pi/2);
+                [actionComplete, basePos] = robot.MoveBase(destination);
+                %check base pos for collision befor plotting 
+                %if collision -> wait for no collision
+                %else -> plot
+                robot.model.base = basePos;
+                robot.model.plot(robot.model.getpos)
+                step = step + actionComplete;
+    
+                % unfold arm
+                % create move function that takes in a joint state
+            elseif step == 2
+                    [actionComplete, qMatrix] = robot.Move(transl(0, 0.5, 0.75)*trotx(pi));
+                    [collision, intersectP, i] = robot.IsArmCollision(qMatrix, environment);
+                    %check arm for collisions befor plotting
+                    %if collisions -> move around
+                    %else -> plot
+                    if collision == 1 && 0
+                        i
+                        intersectP
+                    else
+                        robot.model.plot(qMatrix)
+                        step = step + actionComplete;
+                    end
+        
+                    % pick up first wrench
+            elseif step == 3
+                [actionComplete, qMatrix] = robot.Move(wrench1Pos*trotx(pi));
+                %[collision, intersectP, i] = robot.IsArmCollision(qMatrix, environment);
+                robot.model.plot(qMatrix)
+                step = step + actionComplete;
+        
+                % done
+            elseif step == 4
+                break
             end
         end
     end
-    %check light curtain
-    
-    % move to workbench
-    if step == 1
-        destination = transl(workBenchPos(1, 4), workBenchPos(2, 4) - 0.95, fetchBase(3, 4))*trotz(pi/2);
-        [actionComplete, basePos] = robot.MoveBase(destination);
-        %check base pos for collision befor plotting 
-            %if collision -> wait for no collision
-            %else -> plot
-        robot.model.base = basePos;
-        robot.model.plot(robot.model.getpos)
-        step = step + actionComplete;
-    
-    % unfold arm
-        % create move function that takes in a joint state
-    elseif step == 2
-        [actionComplete, qMatrix] = robot.Move(transl(0, 0.5, 0.75)*trotx(pi));
-        [collision, intersectP, i] = robot.IsArmCollision(qMatrix, environment);
-        %check arm for collisions befor plotting
-            %if collisions -> move around
-            %else -> plot
-        if collision == 1 && 0
-            i
-            intersectP
-        else
-            robot.model.plot(qMatrix)
-            step = step + actionComplete;
-        end
-        
-    % pick up first wrench
-    elseif step == 3
-        [actionComplete, qMatrix] = robot.Move(wrench1Pos*trotx(pi));
-        %[collision, intersectP, i] = robot.IsArmCollision(qMatrix, environment);
-        robot.model.plot(qMatrix)
-        step = step + actionComplete;
-        
-    % done
-    elseif step == 4
-        break
-    end
-    
 end
 
 hold on;
@@ -105,7 +137,7 @@ plot3(workBenchPos(1, 4)-0.3, workBenchPos(2, 4), workBenchPos(3, 4) + 0.1, 'r.'
 hold off;
 
 disp('DONE!')
-end
+
 
 function obj = CreateObject(file, pos)
     splitLine = split(file, '\');
